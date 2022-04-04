4077deb782177d321c563cff8ceefa2e1339e008
hieuntph17602 <77196637+hieuntph17602@users.noreply.github.com>
2022-04-05 03:00:14 +0700

    ass1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
+import { PhoneDetailComponent } from './layouts/clients/phone-detail/phone-detail.component';
+import { PhonesComponent } from './layouts/clients/phones/phones.component';
+import { InforComponent } from './layouts/clients/infor/infor.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './layouts/clients/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+    children: [
+      {
+        path: '',
+        component: InforComponent
+      }
+    ]
+  },
+  {
+    path: 'phones',
+    component: PhonesComponent
+  },
+  {
+    path: 'phones/:id',
+    component: PhoneDetailComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './layouts/clients/header/header.component';
+import { FooterComponent } from './layouts/clients/footer/footer.component';
+import { IndexComponent } from './layouts/clients/index/index.component';
+import { InforComponent } from './layouts/clients/infor/infor.component';
+import { PhonesComponent } from './layouts/clients/phones/phones.component';
+import { PhoneDetailComponent } from './layouts/clients/phone-detail/phone-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    InforComponent,
+    PhonesComponent,
+    PhoneDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
